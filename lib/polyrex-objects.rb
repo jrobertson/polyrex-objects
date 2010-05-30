e9e9b5ad7e8246738cf2124ad4a33094d7eaf418
James Robertson <james@r0bertson.co.uk>
2010-05-30 11:08:28 +0100

    first commit

---
+++ lib/polyrex-objects.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/ruby
+
+# file: polyrex-objects.rb
+
+class PolyrexObjects
+  def initialize(schema)
+
+    a = schema.split('/')
+    a.shift
+    @class_names = []
+
+    a.each do |x|
+      name, raw_fields = x.split('[')
+      fields = raw_fields.chop.split(',').map &:strip
+      @class_names << name.captalize
+
+      classx = []  
+      classx << "class #{name.capitalize}"
+      classx << "def initialize(node); @node = node; end"
+      fields.each do |field|
+        classx << "def #{field}; XPath.first(@node, 'summary/#{field}/text()'); end"
+      end
+      classx << "end"
+
+      eval classx.join("\n")
+    end
+
+  end
+
+  def to_a
+    @class_names.map {|x| eval(x)}
+  end
+end
---
+++ polyrex-objects.gemspec
@@ -0,0 +1,6 @@
+Gem::Specification.new do |s|
+  s.name = 'polyrex-objects'
+  s.version = '0.1.0'
+  s.summary = 'polyrex-objects'
+  s.files = Dir['lib/**/*.rb']
+end
